c31d7edd4f08c5e36de72ad9952ee61100e78d2c
Pedro Frohmut <pedrofrohmut@gmail.com>
2019-06-03 12:02:05 -0300

    Added Blank Web Project to Solution

---
+++ TRMDataManager/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace TRMDataManager.Controllers
+{
+  [Authorize]
+  public class ValuesController : ApiController
+  {
+    // GET api/values
+    public IHttpActionResult Get()
+    {
+      string userId = RequestContext.Principal.Identity.GetUserId();
+      return Ok<IEnumerable<string>>(new string[] { "value1", "value2", userId });
+    }
+
+    // GET api/values/5
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    // POST api/values
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT api/values/5
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE api/values/5
+    public void Delete(int id)
+    {
+    }
+  }
+}
