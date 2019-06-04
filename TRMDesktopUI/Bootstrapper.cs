376b02c1d8d0a8d063d414331e760a2426ab662e
Pedro Frohmut <pedrofrohmut@gmail.com>
2019-06-04 11:34:49 -0300

    Desktop UI Bootstrapper for Caliburn Micro with Simple Container

--- TRMDesktopUI/Bootstrapper.cs
+++ TRMDesktopUI/Bootstrapper.cs
@@ -7,18 +7,56 @@ using System.Threading.Tasks;
 using System.Windows;
 using TRMDesktopUI.ViewModels;
 
+// This Class is based on Caliburn Micro Documentation Bootstrap with Simple Container
+// https://caliburnmicro.com/documentation/simple-container
 namespace TRMDesktopUI
 {
   public class Bootstrapper : BootstrapperBase
   {
-    public Bootstrapper()
+    // Must Have for CaliburnMicro Container to work 
+    private readonly SimpleContainer container = new SimpleContainer();
+
+    public Bootstrapper() { Initialize(); }
+
+    protected override void Configure()
     {
-      Initialize();
+      this.container
+        // Sets what is the instance of container to be used for the App
+        .Instance(this.container);
+
+      this.container
+        // Manage Windows in the App, bringing windows in and out
+        .Singleton<IWindowManager, WindowManager>()
+        // Event container to pass event between different components
+        .Singleton<IEventAggregator, EventAggregator>();
+
+      GetType().Assembly.GetTypes()
+        .Where(type => type.IsClass)
+        .Where(type => type.Name.EndsWith("ViewModel")) // return IEnumerable
+        .ToList()
+        .ForEach(viewModelType => 
+          // interface / key / implementation
+          this.container.RegisterPerRequest(
+            viewModelType, 
+            viewModelType.ToString(),
+            viewModelType
+          )
+        );
     }
 
-    protected override void OnStartup(object sender, StartupEventArgs e)
-    {
+    protected override void OnStartup(object sender, StartupEventArgs e) =>
       DisplayRootViewFor<ShellViewModel>();
-    }
+
+    // Must Have for CaliburnMicro Container to work 
+    protected override object GetInstance(Type service, string key) => 
+      this.container.GetInstance(service, key);
+
+    // Must Have for CaliburnMicro Container to work 
+    protected override IEnumerable<object> GetAllInstances(Type service) => 
+      this.container.GetAllInstances(service);
+
+    // Must Have for CaliburnMicro Container to work 
+    protected override void BuildUp(object instance) => 
+      this.container.BuildUp(instance);
   }
 }
